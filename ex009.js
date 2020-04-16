77c3609b298b8d1ce35cbd355102e5a748a98b1e
wanessabezerra <53651524+wanessabezerra@users.noreply.github.com>
2020-04-16 16:30:21 -0300

    AULA11

---
+++ ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}.`)
+if (pais == 'Brasil') { // Condição simples
+    console.log('Você é Brasileiro(a)!')
+} else {
+    console.log('Você é Estrangeiro(a)!')
+}
