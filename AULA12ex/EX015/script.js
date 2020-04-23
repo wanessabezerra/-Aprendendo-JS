bf0722c5a7e0ec6a26e3ff8646ecf985a91f39a0
wanessabezerra <53651524+wanessabezerra@users.noreply.github.com>
2020-04-23 20:26:12 -0300

    AULA12ex

---
+++ AULA12ex/EX015/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        //Bom Dia!
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#c5b656'
+    } else if (hora >= 12 && hora < 18) {
+        //Boa Tarde!
+        img.src = 'fototarde.png'
+        document.body.style.background = '#d35a08'
+    } else {
+        //Boa Noite!
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#64686f'
+    }
+}
