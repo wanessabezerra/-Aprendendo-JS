ac26938d0b8cddd99ffb97055c2e62c6754a886c
wanessabezerra <53651524+wanessabezerra@users.noreply.github.com>
2020-04-23 20:42:45 -0300

    Delete ex009.js

--- ex009.js
+++
@@ -1,7 +0,0 @@
-var pais = 'EUA'
-console.log(`Vivendo em ${pais}.`)
-if (pais == 'Brasil') { // Condição simples
-    console.log('Você é Brasileiro(a)!')
-} else {
-    console.log('Você é Estrangeiro(a)!')
-}
