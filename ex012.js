40c011847f1f752491acbb5ab9e78e10e27e8aa7
wanessabezerra <53651524+wanessabezerra@users.noreply.github.com>
2020-04-23 20:41:53 -0300

    Delete ex012.js

--- ex012.js
+++
@@ -1,9 +0,0 @@
-var idade = 67
-console.log(`Você tem ${idade} anos`)
-if (idade < 16) {
-    console.log('Não Vota')
-} else if (idade < 18 || idade > 65) {
-    console.log('Voto Opcional')
-} else {
-    console.log('Voto Obrigatório ')
-}
